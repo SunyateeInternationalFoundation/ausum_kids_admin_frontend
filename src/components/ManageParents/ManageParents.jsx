bd62bd5f7177d077994e9fe98cff31ddd77d38df
Kayathri-maths <kayualagarsamy@gmail.com>
2024-12-31 18:26:50 +0530

    changes_style

--- src/components/ManageParents/ManageParents.jsx
+++ src/components/ManageParents/ManageParents.jsx
@@ -1,5 +1,11 @@
 const ManageParents = () => {
-  return <div>ManageParents</div>;
+  return (
+    <div>
+      <h1 className="text-gray-500 text-xl font-bold text-center">
+        Manage Parents
+      </h1>
+    </div>
+  );
 };
 
 export default ManageParents;
